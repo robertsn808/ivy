fe98af4adeb94b6c25832e19fe954469f44755ec
robertsn808 <robertsn@hawaii.edu>
2022-11-09 12:47:59 -1000

    @CerberusLatrans
    I have made those corrections except for the reusing of the out array. Is that sort of like recurrence?

--- ivy/functional/frontends/numpy/ufunc/methods.py
+++ ivy/functional/frontends/numpy/ufunc/methods.py
@@ -9,28 +9,14 @@ from self import self
 
 class ufunc:
 
-    def __accumulate__(array, axis, dtype, out):
+    def __accumulate__(array, dtype, op):
         r = ivy.array(np.empty(len(A)))
         t = op.identity
 
         for i in range(len(A)):
             t = op(t, A[i])
             r[i] = t
-
-        if ivy.exists(out):
-            return ivy.any(self, dtype=dtype, out=out)
-
         return r
-
-
-    # If out != None
-    #def __out__(self, axis=None, dtype=None, out=ndarray):
-     #   if ivy.exists(out):
-      #      r = ufunc.__call__(out)
-       #     return r
-
-    # if axis > 1
-    #def __axis__(self, axis, dtype=None, out=None):
 
 
 
