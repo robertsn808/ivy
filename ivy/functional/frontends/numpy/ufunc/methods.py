db1971ecba4890ec7268125ec655a01366c42e1b
Robert Swanson-Neitzel <robertsn@hawaii.edu>
2022-11-13 10:53:13 -1000

    Update methods.py

--- ivy/functional/frontends/numpy/ufunc/methods.py
+++ ivy/functional/frontends/numpy/ufunc/methods.py
@@ -1,36 +1,15 @@
 from lib2to3.pgen2.grammar import op
-
 import ivy
 import numpy as np
 from etils.edc.dataclass_utils_test import A
-import ivy
-
 
 class ufunc:
-
     def __accumulate__(array, axis, dtype, out):
         r = ivy.array(np.empty(len(A)))
         t = op.identity
-
         for i in range(len(A)):
             t = op(t, A[i])
             r[i] = t
-
         if ivy.exists(out):
             return ivy_test.__call__(self, dtype=dtype, out=out)
-
         return r
-
-
-    # If out != None
-    #def __out__(self, axis=None, dtype=None, out=ndarray):
-     #   if ivy.exists(out):
-      #      r = ufunc.__call__(out)
-       #     return r
-
-    # if axis > 1
-    #def __axis__(self, axis, dtype=None, out=None):
-
-
-
-
