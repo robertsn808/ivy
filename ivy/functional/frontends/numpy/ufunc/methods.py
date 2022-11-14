00f8da233f5a3aeaeb62bd64a7e0ca241b02e5f4
Robert Swanson-Neitzel <robertsn@hawaii.edu>
2022-11-13 22:09:29 -1000

    Update methods.py

--- ivy/functional/frontends/numpy/ufunc/methods.py
+++ ivy/functional/frontends/numpy/ufunc/methods.py
@@ -4,11 +4,10 @@ from etils.edc.dataclass_utils_test import A
 
 
 class ufunc:
-    def __accumulate__(array, dtype, opfunc):
+    def __init__(array, dtype, opfunc):
         r = ivy.array(np.empty(len(A)))
-        op = opfunc
-        t = op.identity
+        t = opfunc.identity
         for i in range(len(A)):
-            t = op(t, A[i])
+            t = opfunc(t, A[i])
             r[i] = t
         return r
