b3aacc905036b4af666ad14599240b11beb5de04
robertsn808 <robertsn@hawaii.edu>
2022-11-14 12:11:56 -1000

    @CerberusLatrans
    Am I getting close or am I way off?

--- ivy/functional/frontends/numpy/ufunc/__init__.py
+++ ivy/functional/frontends/numpy/ufunc/__init__.py
@@ -1,3 +1,15 @@
 # flake8: noqa
+from ivy.ivy import empty
 from . import methods
 from .methods import *
+
+from etils.edc.dataclass_utils_test import A
+
+from .. import identity
+
+def accumulate(ivy.array, ivy.identity):
+    for i in range(len(A)):
+        t = op(t, A[i])
+        r[i] = t
+    return r
+    return
