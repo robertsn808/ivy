3d5b035030b134e8c6b20c1e2b202a96bf484cd3
robertsn808 <robertsn@hawaii.edu>
2022-11-15 21:50:21 -1000

    @CerberusLatrans
    Is this what you mean?

--- ivy/functional/frontends/numpy/ufunc/methods.py
+++ ivy/functional/frontends/numpy/ufunc/methods.py
@@ -3,9 +3,12 @@ from etils.edc.dataclass_utils_test import A
 
 
 class ufunc:
-    def __init__(self, dtype, opfunc):
-        def accumulate(r=np.empty(len(A)), op=opfunc.identity):
-            for i in range(len(A)):
-                t = op(t,A[i])
-                r[i] = t
-            return r
+    def __init__(self, array, op):
+        self.array = array
+        self.operator = op.identity()
+
+    def accumulate(self, op):
+        r = np.empty(len(self.array.length))
+        for i in range(len(self.array.length)):
+            self.operator = op(self.operator, A[i])
+            r[i] = self.operator
